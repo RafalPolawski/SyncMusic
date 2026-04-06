44cd699f271bc73fc77c63aed7d87b61b657c644
Rafał Poławski <rafalpolawski01@gmail.com>
2026-04-07 01:04:14 +0200

    fix(devops): fix postgres credentials and db initialization

--- caddy/init-db.sql
+++ caddy/init-db.sql
@@ -1,2 +1,16 @@
-SELECT 'CREATE DATABASE syncmusic'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'syncmusic')\gexec
+-- This script runs as the 'postgres' user
+CREATE DATABASE syncmusic;
+
+-- Ensure the keycloak user exists with the correct password from environment (if we could, but SQL is static)
+-- So we'll just create it with a placeholder if it doesn't exist, though POSTGRES_USER usually handles one.
+-- Actually, the best way for DevOps is to have the APP users separate.
+
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'keycloak') THEN
+        CREATE USER keycloak WITH PASSWORD 'syncmusic_db_secret';
+    END IF;
+END
+$$;
+
+GRANT ALL PRIVILEGES ON DATABASE keycloak TO keycloak;
